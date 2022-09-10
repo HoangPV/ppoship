3d8f87f58fbcc537c65f3c0db96328805e74f25a
Hoang Phan <hoangpv@smartosc.com>
2022-09-10 13:07:31 -0700

    PPO-39 Block "Testimonial"

--- web/themes/custom/ppoship_theme/js/partner.js
+++ web/themes/custom/ppoship_theme/js/partner.js
@@ -10,11 +10,25 @@
    });
  }
 
+ function doSlickTestimonial(elm) {
+   $(elm).slick({
+     slidesToShow: 2,
+     slidesToScroll: 2,
+     centerMode: true,
+     focusOnSelect: true
+   });
+ }
+
   Drupal.behaviors.myBehavior = {
     attach: function (context, settings) {
       once('myBehavior', '.view-block-partner .view-content', context).forEach(
         function (element) {
           doSlick(element);
+        }
+      );
+      once('myBehavior', '.view-testimonial .view-content', context).forEach(
+        function (element) {
+          doSlickTestimonial(element);
         }
       );
     }
